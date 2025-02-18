5a8e6c67c040e6e0882e4127840d09c008dfda01
Gleb <lekomcevg@gmail.com>
2025-02-18 09:59:15 +0300

    fix remove blocks

--- src/test/java/demoqa/pages/BasePage.java
+++ src/test/java/demoqa/pages/BasePage.java
@@ -3,20 +3,26 @@ package demoqa.pages;
 
 import com.codeborne.selenide.Selenide;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
 public class BasePage {
     public void removeBlocks(){
-        // Скрытие элемента с id "fixedban"
         try {
+            $("#fixedban").shouldNotBe(visible);
             Selenide.executeJavaScript("document.getElementById('fixedban').style.display = 'none';");
         } catch (Exception e) {
             System.out.println("Элемент с id 'fixedban' не найден или уже скрыт");
         }
 
-        // Скрытие футера
         try {
+            $("footer").shouldNotBe(visible);
             Selenide.executeJavaScript("document.querySelector('footer').style.display = 'none';");
         } catch (Exception e) {
             System.out.println("Футер не найден или уже скрыт");
         }
+
+
+
     }
 }
