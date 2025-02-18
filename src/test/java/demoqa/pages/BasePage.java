04ca023c71e24197911c6c9471fec384bfdba67f
Gleb <lekomcevg@gmail.com>
2025-02-18 09:44:59 +0300

    fix remove blocks

--- src/test/java/demoqa/pages/BasePage.java
+++ src/test/java/demoqa/pages/BasePage.java
@@ -1,23 +1,20 @@
 package demoqa.pages;
 
-import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.cssValue;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class BasePage {
     public void removeBlocks(){
         try {
-            $(By.cssSelector("#fixedban")).should(exist);
-            executeJavaScript("document.querySelector('#fixedban').style.display='none';");
+            $("#fixedban").shouldBe(visible).shouldHave(cssValue("display", "none"));
         } catch (Exception e) {
             System.out.println("Элемент с id 'fixedban' не найден");
         }
 
         try {
-            $(By.tagName("footer")).should(exist);
-            executeJavaScript("document.querySelector('footer').style.display='none';");
+            $("footer").shouldBe(visible).shouldHave(cssValue("display", "none"));
         } catch (Exception e) {
             System.out.println("Футер не найден");
         }
