8626a0fa77c213c07bce8521242717c5c792e421
Gleb <lekomcevg@gmail.com>
2025-02-18 12:29:06 +0300

    fix all

--- src/test/java/demoqa/tests/BaseTest.java
+++ src/test/java/demoqa/tests/BaseTest.java
@@ -1,6 +1,5 @@
 package demoqa.tests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
