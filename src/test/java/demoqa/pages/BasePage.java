90de33b63706f86854a3b668bcf9c05e71948983
Gleb <lekomcevg@gmail.com>
2025-02-25 16:01:30 +0300

    fix blocks

--- src/test/java/demoqa/pages/BasePage.java
+++ src/test/java/demoqa/pages/BasePage.java
@@ -2,10 +2,12 @@ package demoqa.pages;
 
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.hidden;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.switchTo;
 
 public class BasePage {
     public void removeBlocks() {
@@ -25,5 +27,20 @@ public class BasePage {
         } catch (Exception e) {
             System.out.println("Footer не найден или уже скрыт");
         }
+
+        try {
+            // Переключаемся на iframe, идентификатор которого начинается с 'google_ads_iframe_'
+            SelenideElement iframeElement = $("iframe[id^='google_ads_iframe_']");
+            iframeElement.shouldBe(visible);
+            switchTo().frame(iframeElement);
+            Selenide.executeJavaScript("arguments[0].style.setProperty('display', 'none', 'important');", iframeElement);
+            switchTo().defaultContent();
+            iframeElement.shouldBe(hidden);
+        } catch (Exception e) {
+            System.out.println("Iframe не найден или уже скрыт: " + e.getMessage());
+        } finally {
+            switchTo().defaultContent();
+        }
+
     }
 }
